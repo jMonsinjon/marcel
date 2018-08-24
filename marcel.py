de3d65f58a6b9950e0104ef29de4c7c1fab16bea
Jérémie <jeremie.monsinjon-ext@isocel.info>
2018-08-24 11:38:17 +0200

    Ajout d'un helper

--- marcel.py
+++ marcel.py
@@ -120,6 +120,74 @@ def use_marcelfile(command):
     return command
 
 
+def print_marcelhelp():
+    """Build the help message to print as an executable command."""
+    command = ['echo']
+    command = command + ['Utilisation:   marcel [OPTIONS] COMMAND ', '\n']
+    command = command + ['\n']
+    command = command + ['Le Docker à la française !', '\n']
+    command = command + ['\n']
+    command = command + ['Options :', '\n']
+    command = command + ['      --aide                          ', 'Vient au secoursde l\'utilisateur', '\n']
+    command = command + ['      --auteur                        ', 'Permet de préciser le nom du propriétaire intellectuel de l\'oeuvre', '\n']
+    command = command + ['      --sortie                        ', '--output', '\n']
+    command = command + ['      --49-3                          ', 'Décision unilatérale permettant d\'assurer l\'exécution de la procédure', '\n']
+    command = command + ['      --etat-d-urgence                ', 'Desormais, je fais ce qu\'il me plait', '\n']
+    command = command + ['      --disque-numerique-polyvalent   ', '--dvd', '\n']
+    command = command + ['\n']
+
+    command = command + ['Groupes de directives :', '\n']
+    # command = command + ['  checkpoint            ', 'Gestion des  checkpoints', '\n']
+    # command = command + ['  config                ', 'Gestion des  Docker configs', '\n']
+    command = command + ['  récipient             ', 'Placard', '\n']
+    command = command + ['  cédérom               ', 'Médiathèque', '\n']
+    command = command + ['  réseau                ', 'Point de rassemblement', '\n']
+    # command = command + ['  node                  ', 'Gestion des  Swarm nodes', '\n']
+    # command = command + ['  plugin                ', 'Gestion des  plugins', '\n']
+    # command = command + ['  secret                ', 'Gestion des  Docker secrets', '\n']
+    # command = command + ['  service               ', 'Gestion des  services', '\n']
+    # command = command + ['  stack                 ', 'Gestion des  Docker stacks', '\n']
+    # command = command + ['  swarm                 ', 'Gestion des  Swarm', '\n']
+    # command = command + ['  system                ', 'Gestion des  Docker', '\n']
+    # command = command + ['  trust                 ', 'Gestion des  trust on Docker images', '\n']
+    command = command + ['  monte-le-son          ', 'Table de mixage', '\n']
+    command = command + ['\n']
+
+    command = command + ['Directives :', '\n']
+    command = command + ['  chauffe               ', 'Démarre un nouveau récipient', '\n']
+    command = command + ['  fais                  ', 'Exécute une nouvelle directive dans un récipient existant', '\n']
+    command = command + ['  pousse                ', 'Envoi un cédérom dans la médiathèque', '\n']
+    command = command + ['  apporte               ', 'Récupère un cédérom de la médiathèque', '\n']
+    command = command + ['  bûches                ', 'Invite les bûches d\'un récipient', '\n']
+    command = command + ['  grève                 ', 'Arrêt du travail pour cause de revendications mutiples. Sortez les barbecues !', '\n']
+    command = command + ['  matuer                ', 'Assassine sauvagement un récipient', '\n']
+    command = command + ['  perquisitionne        ', 'Récupère les informations de chaque objet Marcel', '\n']
+    command = command + ['  construis             ', 'Grave un nouveau cédérom', '\n']
+    command = command + ['  charge                ', 'Copie un cédérom depuis un disque local', '\n']
+    # command = command + ['  plagie                ', 'copy', '\n']
+    command = command + ['  france24              ', 'Retransmet les informations essentielles à propos de Marcel', '\n']
+    # command = command + ['  insee                 ', 'stats', '\n']
+    command = command + ['  rtt                   ', 'Donne une pause bien méritée au récipient', '\n']
+    # command = command + ['  sur-ecoute            ', 'attach', '\n']
+    command = command + ['  cederoms              ', 'Affiche la liste des cédéroms de la médiathèque locale', '\n']
+    command = command + ['  vos-papiers           ', 'Montre pâte blanche auprès de ces messieurs les gendarmes', '\n']
+    # command = command + ['  déchéance             ', 'logout', '\n']
+    command = command + ['  sauvegarde            ', 'Copie un cédérom sur un support amovible', '\n']
+    command = command + ['  graffiti              ', 'Dessine un nom sur un cédérom', '\n']
+    command = command + ['  rsa                   ', 'Supprime un cédérom de la médiathèque locale', '\n']
+    command = command + ['  assigne-à-résidence   ', 'Grave un cédérom à partir d\'un récipient existant', '\n']
+    command = command + ['  roman-national        ', 'Affiche toute l\'histoire d\'un cédérom', '\n']
+    command = command + ['  recycle               ', 'Supprime un récipient', '\n']
+    command = command + ['  cherche               ', 'Envoi Lycos pour trouver un cédérom à la médiathèque du coin de la rue', '\n']
+    # command = command + ['  marseille             ', 'port', '\n']
+    # command = command + ['  renomme               ', 'rename', '\n']
+    command = command + ['  auboulot              ', 'Remet au boulot un récipient en grève', '\n']
+    command = command + ['  barrage               ', 'Empêche un récipient d\'avancer jusqu\'à ce qu\'il capitule', '\n']
+    command = command + ['  socialistes           ', 'Affiche la liste des récipients actifs', '\n']
+    command = command + ['  aide                  ', 'Vient au secours de l\'utilisateur', '\n']
+    return command
+    
+
 def replace_command(command):
     """Replace the executable itself for given values of the first command."""
     if len(command) > 1 and command[1] == 'et-son-orchestre':
@@ -127,6 +195,9 @@ def replace_command(command):
         command[0] = 'docker-compose'
     else:
         command[0] = 'docker'
+
+    if len(command) == 1:
+        command.append('help')
     return command
 
 
@@ -143,6 +214,8 @@ def build_command(command):
         subcommand = command[1]
         if subcommand == 'build':
             command = use_marcelfile(command)
+        elif subcommand == 'help':
+            command = print_marcelhelp()
     return command
 
 
